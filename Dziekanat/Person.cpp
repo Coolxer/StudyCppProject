0b3abf7f7cb8720bb9bdc02f5b9c91106b8f6aff
coolxer <lukasz.milos@int.pl>
2020-01-02 21:01:56 +0100

    add Person class

---
+++ Dziekanat/Person.cpp
@@ -0,0 +1,18 @@
+#include "Person.h"
+
+Person::Person(std::string firstName, std::string lastName, int age)
+{
+	this->firstName = firstName;
+	this->lastName = lastName;
+	this->age = age;
+}
+
+std::string Person::getFirstName() const
+{
+	return this->firstName;
+}
+
+std::string Person::getLastName() const
+{
+	return this->lastName;
+}
---
+++ Dziekanat/Person.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+
+class Person
+{
+private:
+	std::string firstName;
+	std::string lastName;
+	int age;
+public:
+	Person(){}
+	Person(std::string firstName, std::string lastName, int age);
+
+	std::string getFirstName() const;
+	std::string getLastName() const;
+};
+
